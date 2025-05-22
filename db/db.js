bfb2b1ac0ae347a9e28f0a095b491dbc15901f50
moh0x <141264101+moh0x@users.noreply.github.com>
2025-05-22 09:46:00 +0000

    Update db.js

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDb = async()=>{
     try {
-        await mongoose.connect("mongodb+srv://sogaimohamedamine:<db_password>@cluster0.dhfkbeg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect("mongodb+srv://highdep603:2k4MnozzJUKWqb6c@highdep.fegh8bn.mongodb.net/?retryWrites=true&w=majority&appName=highdep");
         console.log('db connected');
         
     } catch (error) {
